f4d73eab0a3023524fd0fba1cfec9cb132b81e5a
duanmuseventeen <51553166+duanmuseventeen@users.noreply.github.com>
2024-11-28 20:28:01 +0800

    Add files via upload

---
+++ scRNAseq/5.scRNAseq-hsa-heart-GSE183852.R
@@ -0,0 +1,28 @@
+# Single-cell transcriptomics reveals cell-type-specific diversification in human heart failure----
+# https://pubmed.ncbi.nlm.nih.gov/35959412/
+# GSE183852
+  
+# RefMerge@meta.data$Names is cell type
+# RefMerge@meta.data$condition is group information
+
+# Load data
+# Download from https://explore.data.humancellatlas.org/projects/135f7f5c-4a85-4bcf-9f7c-4f035ff1e428
+load(GSE183852_DCM_Integrated.Robj)
+
+# Visualization
+DimPlot(RefMerge, reduction = "harmony",
+              group.by = "Names", 
+              label = TRUE, pt.size = 0.5) + NoLegend()
+FeaturePlot(RefMerge, features = c(Targetgene))
+VlnPlot(object = RefMerge, features = Targetgene, slot = "counts", log = TRUE, group.by = "Names")
+
+# Subset
+RefMerge <- RegroupIdents(RefMerge, metadata = "Names")
+CM <- subset(RefMerge, subset = Names == "CM")
+
+Targetgene <- CM@assays$RNA$counts[rownames(CM@assays$RNA$counts) == Targetgene]
+group <- CM@meta.data$condition
+
+mean(Targetgene[group == "Donor"])
+mean(Targetgene[group != "Donor"])
+wilcox.test(Targetgene ~ group)
