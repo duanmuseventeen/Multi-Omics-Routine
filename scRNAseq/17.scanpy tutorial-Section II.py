02c18370a7df09e213e574204cf8ef054f1477e6
duanmuseventeen <51553166+duanmuseventeen@users.noreply.github.com>
2024-12-11 23:16:47 +0800

    Update 17.scanpy tutorial-Section II.py

--- scRNAseq/17.scanpy tutorial-Section II.py
+++ scRNAseq/17.scanpy tutorial-Section II.py
@@ -25,7 +25,7 @@ sc.settings.set_figure_params(dpi=80, facecolor="white")
 results_file = "write/pbmc3k.h5ad"  # the file that will store the analysis results
 
 # Read in the count matrix into an AnnData object, which holds many slots for annotations and different representations of the data. It also comes with its own HDF5-based file format: .h5ad.
-
+# 使用绝对路径，相对路径报错
 adata = sc.read_10x_mtx(
   "data/filtered_gene_bc_matrices/hg19/",  # the directory with the `.mtx` file
   var_names="gene_symbols",  # use gene symbols for the variable names (variables-axis index)
