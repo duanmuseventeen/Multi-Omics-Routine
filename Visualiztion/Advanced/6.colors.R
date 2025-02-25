9b8ac999e549869be7735f89c58aec7299c77336
duanmuseventeen <51553166+duanmuseventeen@users.noreply.github.com>
2025-02-25 14:50:32 +0800

    Add files via upload

---
+++ Visualiztion/Advanced/6.colors.R
@@ -0,0 +1,13 @@
+# gradient
+colorRampPalette(c("white","#007947"))
+                
+colorRampPalette(c("white","red3"))        
+
+# for single cell
+# https://github.com/friedpine/scRNASeq_ESCC/blob/main/3c.R
+mycol <-c("#DEEAB2","#64B473","#2D553C","#A1D8C9","#487C76","#7AAF93","#D0E4C6",
+          "#F3746C","#BB4B95","#F8BFAF","#F7DDD3","#66CDF6","#598198","#D5E7F7",
+          "#69B3CE","#D6D5EB","#7B8CBC","#7674AE", "#E3CEE4",
+          "#AFB2B6","#C9BDB2","#F5C785","#ECAECF","#E9A943","#CAA57D","#A79388",
+          "#EACF68","#F6F3A7","#C45337","#86382B","#EADCE4",
+          "#EE5276","#9E6CAB","#74507B")
