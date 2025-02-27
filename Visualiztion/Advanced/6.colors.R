da49f2ced16025d0515ac9ccc41ecad30ef41657
duanmuseventeen <51553166+duanmuseventeen@users.noreply.github.com>
2025-02-27 12:17:34 +0800

    Update 6.colors.R

--- Visualiztion/Advanced/6.colors.R
+++ Visualiztion/Advanced/6.colors.R
@@ -11,3 +11,14 @@ mycol <-c("#DEEAB2","#64B473","#2D553C","#A1D8C9","#487C76","#7AAF93","#D0E4C6",
           "#AFB2B6","#C9BDB2","#F5C785","#ECAECF","#E9A943","#CAA57D","#A79388",
           "#EACF68","#F6F3A7","#C45337","#86382B","#EADCE4",
           "#EE5276","#9E6CAB","#74507B")
+
+# https://mp.weixin.qq.com/s?__biz=MzkwNTY1ODMwMw==&mid=2247487820&idx=1&sn=97bff731a76911156492ca40521d0ef9&chksm=c0f53b9cf782b28ac865935db146048ebe0146035f97f388672f5597a0f7ef6021ea9681e5c3&scene=21#wechat_redirect
+c("#B2182B","#E69F00","#56B4E9","#009E73","#F0E442",
+  "#0072B2","#D55E00","#CC79A7","#CC6666",
+  "#9999CC","#66CC99","#3C5488B2","#00A087B2", 
+  "#F39B7FB2","#91D1C2B2", 
+  "#8491B4B2", "#DC0000B2", 
+  "#7E6148B2","yellow", 
+  "darkolivegreen1", "lightskyblue")
+
+
