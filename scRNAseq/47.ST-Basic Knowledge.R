87c9b33585d2d8c04f59272d1d543d93feace680
duanmuseventeen <51553166+duanmuseventeen@users.noreply.github.com>
2025-05-10 17:21:28 +0800

    Add files via upload

---
+++ scRNAseq/47.ST-Basic Knowledge.R
@@ -0,0 +1,45 @@
+# Theory:
+# https://www.bilibili.com/video/BV1FT421Q7Xs/?spm_id_from=333.1007.top_right_bar_window_default_collection.content.click&vd_source=7687d926bc3779b048e880123e66bca4
+# https://www.bilibili.com/video/BV1Uj411D7Le?spm_id_from=333.788.videopod.sections&vd_source=7687d926bc3779b048e880123e66bca4
+# https://www.bilibili.com/video/BV1Qa4y1375K?spm_id_from=333.788.videopod.sections&vd_source=7687d926bc3779b048e880123e66bca4
+# https://www.bilibili.com/video/BV1eH4y1N7Ge?spm_id_from=333.788.videopod.sections&vd_source=7687d926bc3779b048e880123e66bca4
+# https://www.bilibili.com/video/BV1oJ411r7e7/?spm_id_from=333.337.search-card.all.click&vd_source=7687d926bc3779b048e880123e66bca4
+# https://www.bilibili.com/video/BV1KJ411k7R2?spm_id_from=333.788.videopod.sections&vd_source=7687d926bc3779b048e880123e66bca4
+
+# https://www.youtube.com/watch?v=fCd6B5HRaZ8&t=1s
+# https://www.youtube.com/watch?v=dlz3q3Bq5B4
+
+# Offical:
+# https://satijalab.org/seurat/articles/visiumhd_analysis_vignette
+# https://www.10xgenomics.com/support/software/space-ranger/latest/analysis/outputs/output-overview
+# https://www.10xgenomics.com/support/software/loupe-browser/latest
+# https://www.10xgenomics.com/support/software/loupe-vdj-browser/latest
+# https://satijalab.org/seurat/articles/visiumhd_analysis_vignette
+# https://satijalab.org/seurat/articles/spatial_vignette
+
+# Other
+# https://www.youtube.com/watch?v=wBZhG1pVcT4
+
+# Article:
+# pmid: 30923225
+# Slide-seq: A Scalable Technology for Measuring Genome-Wide Expression at High Spatial Resolution
+# pmid: 
+
+# load pkgs---------------------------------------------------------------------
+# packages required for Visium HD
+# install.packages("hdf5r")
+# install.packages("arrow")
+library(Seurat)
+library(ggplot2)
+library(patchwork)
+library(dplyr)
+
+
+
+
+
+
+
+
+
+
