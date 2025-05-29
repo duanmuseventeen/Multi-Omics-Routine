ca87cb1d92941bd7fbfcdb82418f6d6d073535db
duanmuseventeen <51553166+duanmuseventeen@users.noreply.github.com>
2025-05-30 01:01:37 +0800

    Add files via upload

--- Visualiztion/29.density ridge plot.R
+++ Visualiztion/29.density ridge plot.R
@@ -1,7 +1,14 @@
-# ridge plot
+# https://wilkelab.org/ggridges/articles/introduction.html
 
 require(ggridges)
 
 iris |>
   ggplot(aes(x = Sepal.Length, y = Species, fill = Species)) + 
-  geom_density_ridges(scale = 1)
+  geom_density_ridges(scale = 1) + 
+  scale_fill_gradientn(colours = rev(brewer.pal(11,"RdYlBu")))
+
+iris |>
+  ggplot(aes(x = Sepal.Length, y = Species, fill = stat(x))) + 
+  geom_density_ridges_gradient(scale = 1) +
+  scale_fill_viridis_c(name = "Temp. [F]", option = "C")
+
