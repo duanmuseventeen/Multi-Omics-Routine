b45d3394e16bbe032ca903b171eab5a07f081ef9
duanmuseventeen <51553166+duanmuseventeen@users.noreply.github.com>
2025-06-25 23:19:56 +0800

    Add files via upload

--- scRNAseq/53.Trajectory Analysis-TSCAN.R
+++ scRNAseq/53.Trajectory Analysis-TSCAN.R
@@ -22,7 +22,6 @@ plotmclust(lpsmclust)
 lpsorder <- TSCANorder(lpsmclust)
 lpsorder
 
-# some cells cannot be assign a pseudotime
 dim(lpsorder)
 # [1] 5997    3
 dim(fig)
@@ -30,17 +29,37 @@ dim(fig)
 class(lpsorder)
 # [1] "data.frame"
 
+fig$id <- rownames(fig@meta.data)
+fig.sub <- subset(fig, subset = id %in% lpsorder$sample_name)
+meta <- fig@meta.data
+lpsorder$id <- lpsorder$sample_name
+meta <- meta %>% 
+  left_join(lpsorder, by = "id")
+meta <- as.data.frame(meta)
+rownames(meta) <- meta$id
+fig@meta.data <- as.data.frame(meta)
+DimPlot(fig, group.by = "cell_type")
+DimPlot(fig, group.by = "State")
+FeaturePlot(fig, features = "Pseudotime")
+
 myorder <- lpsorder$Pseudotime
 names(myorder) <- lpsorder$sample_name
 # The tutorial is wrong
 diffval <- difftest(procdata, myorder)
-# take very long time...
-diffval[diffval$qval < 0.05, ]
+diffval[diffval$FDR < 0.05, ]
+#         pval           FDR
+# PTGS2   0.000000e+00  0.000000e+00
+# IL1R2   0.000000e+00  0.000000e+00
+# HLA-B   0.000000e+00  0.000000e+00
+# ACTB    0.000000e+00  0.000000e+00
+# NAMPT  1.595832e-321 6.111399e-318
+# S100A9 4.057465e-319 1.294872e-315
 
 # Use singlegeneplot function to plot the expression value of a single gene against
 # a given pseudotime. Notice that here orderonly should be set to FALSE.
-STAT2expr <- log2(lpsdata["STAT2",]+1)
-singlegeneplot(STAT2expr, TSCANorder(lpsmclust,flip=TRUE,orderonly=FALSE))
+PTGS2expr <- log1p(fig["PTGS2",])
+singlegeneplot(PTGS2expr, TSCANorder(lpsmclust,orderonly=TRUE,flip=TRUE))
+# 错误于exprdata$State: $ operator is invalid for atomic vectors
 # Comparing Different Pseudotemporal Ordering
 # Not Run
 # Where to use it?
