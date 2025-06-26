3dac2d5e790f8f9cc07f783e883d3435be516990
duanmuseventeen <51553166+duanmuseventeen@users.noreply.github.com>
2025-06-26 18:07:59 +0800

    Add files via upload

--- scRNAseq/55.Reference-Neutrophil.R
+++ scRNAseq/55.Reference-Neutrophil.R
@@ -96,3 +96,52 @@ scobj <- subset(scobj, subset = cluster != "Cont")
 
 Xie.2020 <- scobj 
 save(Xie.2020, file = "C:/D/R project/Multi-Omics-Routine/scRNAseq/Neutrophil-Xie.2020.Rdata")
+
+
+# Single-Cell Transcriptomics of Human and Mouse Lung Cancers Reveals Conserved Myeloid Populations across Individuals and Species# 2019
+# Immunity
+# PMID: 30979687
+# GSE127465
+# Load data---------------------------------------------------------------------
+dat <- Matrix::readMM("Zilionis/GSE127465_human_counts_normalized_54773x41861.mtx.gz")
+
+class(data)
+# [1] "function"
+dim(dat)
+# [1] 54773 41861
+class(dat)
+# [1] "dgTMatrix"
+# attr(,"package")
+# [1] "Matrix"
+
+# add gene names
+var <- data.table::fread('Zilionis/GSE127465_gene_names_human_41861.tsv.gz', header = FALSE)
+
+# add per-cell metadata
+meta<- data.table::fread('Zilionis/GSE127465_human_cell_metadata_54773x25.tsv.gz')
+# Create Seurat Object----------------------------------------------------------
+colnames(dat) <- var$V1
+rownames(dat) <- paste0(meta$Library, "_", meta$Barcode)
+scobj <- CreateSeuratObject(counts = t(dat), data = t(dat))
+scobj <- AddMetaData(object = scobj, metadata = meta)
+scobj$nCount_RNA <- scobj@meta.data$`Total counts`
+scobj$perent.mt <- scobj@meta.data$`Percent counts from mitochondrial genes`
+scobj$major_cell_type <- scobj@meta.data$`Major cell type`
+scobj$minor_cell_type <- scobj@meta.data$`Minor subset`
+
+VlnPlot(scobj, features = 'nFeature_RNA')
+VlnPlot(scobj, features = 'nCount_RNA')
+VlnPlot(scobj, features = 'perent.mt')
+
+scobj <- subset(scobj, subset = major_cell_type %in% c("bNeutrophils", "tNeutrophils"))
+
+table(scobj$major_cell_type)
+# bNeutrophils tNeutrophils 
+# 8911         2728 
+
+table(scobj$minor_cell_type)
+# bN1  bN2  bN3  bN4  bN5  bN6  tN1  tN2  tN3  tN4  tN5 
+# 1331  646 2021 3387 1030  496  526   86  425  789  902
+
+Zilions.2019 <- scobj 
+save(Zilions.2019, file = "C:/D/R project/Multi-Omics-Routine/scRNAseq/Neutrophil-Zilions.2019.Rdata")
