74292e37777f72ffacb34850f6b1b48ead728a95
duanmuseventeen <51553166+duanmuseventeen@users.noreply.github.com>
2025-06-26 14:02:07 +0800

    Update 54.Trajectory Analysis-Monocle.R

--- scRNAseq/54.Trajectory Analysis-Monocle.R
+++ scRNAseq/54.Trajectory Analysis-Monocle.R
@@ -73,7 +73,8 @@ cds <- detectGenes(cds, min_expr = 0.1)
 # Constructing Trajectory
 diff_test_res <- differentialGeneTest(
   cds[expressed_genes,],
-  fullModelFormulaStr = "~sm.ns(Pseudotime)")
+  fullModelFormulaStr = "~sm.ns(Pseudotime) + sample",
+  reducedModelFormulaStr = "~sample")
 ordering_genes <- row.names(subset(diff_test_res, qval < 0.01))
 cds <- setOrderingFilter(cds, ordering_genes)
 plot_ordering_genes(cds)
