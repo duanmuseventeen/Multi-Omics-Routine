2d3d264ec8070b6d012af4b7842d3bb2472f4e9e
Duanmu Seventeen <51553166+duanmuseventeen@users.noreply.github.com>
2025-08-18 00:28:17 +0800

    Update 60.Trajectory Analysis-PAGA.R

--- scRNAseq/60.Trajectory Analysis-PAGA.R
+++ scRNAseq/60.Trajectory Analysis-PAGA.R
@@ -1,2 +1,7 @@
 # https://www.jianshu.com/p/e21e7ad6cb9e
+
 # https://nbisweden.github.io/workshop-scRNAseq/labs/scanpy/scanpy_07_trajectory.html
+
+As you can see, we can manipulate the trajectory quite a bit by selecting different number of neighbors, components etc. to fit with our assumptions on the development of these celltypes.
+
+Please explore further how you can tweak the trajectory. For instance, can you create a PAGA trajectory using the orignial umap from Seurat instead? Hint, you first need to compute the neighbors on the umap.
