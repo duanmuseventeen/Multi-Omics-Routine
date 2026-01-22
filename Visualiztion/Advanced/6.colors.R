84a4d25de98c03aa509c7158a34958ace21e85da
Duanmu Seventeen <51553166+duanmuseventeen@users.noreply.github.com>
2026-01-22 08:41:44 +0800

    Update 6.colors.R

--- Visualiztion/Advanced/6.colors.R
+++ Visualiztion/Advanced/6.colors.R
@@ -5,10 +5,8 @@ colorRampPalette(c("white","#007947"))(200)
                 
 colorRampPalette(c("white","red3"))(200)        
 
-# warm (PMID: 40345201)
+# (PMID: 40345201)
 c("black","red","orange")
-
-# cold (PMID: 40345201)
 c("black","blue","green")
 
 # warm (PMID: 38458196)
@@ -16,6 +14,31 @@ colorRampPalette(
     c('gray90','#FFCA62','#FFB336','#FF9700','#FF5A00','#F24410',
       '#E52C22','#DD1D23','#C20030','#930039','#8C003A',
       '#6F003D','#56033F'))(1000)
+
+# (PMID: 38959864)
+warmcol <- c("#FF5733", #(a deep orange-red)
+             "#B22222", #"firebrick",
+             #"#FFA07A", #(a light salmon pink)
+             #"#FFDAB9", #(a peachy beige)
+             "#FFC0CB", #"pink"
+             "#FFC300", #(a bright yellow-orange)
+             "#FF8C00") #(a bright orange)
+
+coldcol <- c(#"turquoise2",
+  #"#66CDAA", #"aquamarine3"
+  #"#7FFFD4",#"aquamarine",
+  #"#00BFFF",#"medium blue",
+  #"#4169E1",#"royal blue",
+  "#4682B4",#, "steelblue",
+  "#6A5ACD",# "slateblue,
+  #"#7FFFD4",#"light turquoise",
+  "#B0E0E6",# "powderblue",
+  #"#00FFFF",#"bright cyan", 
+  #"#E0FFFF", #"pale blue-green"
+  #"#6a51a3", #medianpurple4
+  "#5F9EA0", #"cadetblue"
+  "skyblue2"
+)
 
 # for single cell -----------------------------------------------------------------
 # https://github.com/friedpine/scRNASeq_ESCC/blob/main/3c.R
@@ -60,6 +83,7 @@ new_colors_list = {
  'Red blood cell': '#f7b6d2',
  'Neutrophil': '#dbdb8d'
 }
+
 
 
 
