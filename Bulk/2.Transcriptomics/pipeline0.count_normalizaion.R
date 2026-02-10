f2224f8c6e75033d54124d376405ecae002af7aa
Duanmu Seventeen <51553166+duanmuseventeen@users.noreply.github.com>
2026-02-10 19:09:54 +0800

    Update pipeline0.count_normalizaion.R

--- Bulk/2.Transcriptomics/pipeline0.count_normalizaion.R
+++ Bulk/2.Transcriptomics/pipeline0.count_normalizaion.R
@@ -17,3 +17,13 @@ fpkmToTpm <- function(fpkm){
   exp(log(fpkm) - log(sum(fpkm)) + log(1e6))
 }
 
+
+library(GenomicFeatures)
+## 1.读取GTF文件
+txdb <- makeTxDbFromGFF("gencode.v36.annotation.gtf")
+## 2.提取外显子信息
+exonic <- exonsBy(txdb, by="gene")
+## 3.外显子长度求和
+exonic.gene.sizes <- sum(width(reduce(exonic)))
+## 4.结果整理
+mygeneids <- data.frame(gene_id=names(exonic.gene.sizes),width=exonic.gene.sizes)
