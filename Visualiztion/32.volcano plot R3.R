18cb4907966287445fa3e438b1627b071aeb217a
Duanmu Seventeen <51553166+duanmuseventeen@users.noreply.github.com>
2026-03-07 20:07:02 +0800

    Update 32.volcano plot R3.R

--- Visualiztion/32.volcano plot R3.R
+++ Visualiztion/32.volcano plot R3.R
@@ -2,6 +2,7 @@ require(ggtext)
 require(ggplot2)
 require(dplyr)
 
+# style 1 --------------------------------------------------------------------
 p <- readxl::read_excel("***.xlsx") %>% 
   mutate(sig = case_when(FC >= log2(1.5) & `p-value` < 0.05 ~ 2,
                          FC <=-log2(1.5) & `p-value` < 0.05 ~ 1,
@@ -26,4 +27,23 @@ p <- readxl::read_excel("***.xlsx") %>%
         legend.position = "top",
         legend.background = element_blank())
 
+
 ggsave(plot = p, filename = "***.pdf", path = "", device = "pdf", width = 5, height = 5, dpi = 300)
+
+# style 2 --------------------------------------------------------------------
+# gradient fill suitable for mild but significant change
+data4vol %>% 
+  mutate(sig = case_when(log2FC >=  1 & fdr < 0.05 ~ 2,
+                         log2FC <= -1 & fdr < 0.05 ~ 1,
+                       TRUE ~ 0)) %>% 
+  ggplot(aes(x = log2FC, y = -log10(fdr), fill = log2FC)) +
+  geom_vline(xintercept = c(-1,1), linetype = 2, col = "gray90") +
+  geom_hline(yintercept = -log10(0.05), linetype = 2, col = "gray90") +
+  geom_point(col = "#111111", shape = 21) +
+  scale_fill_gradient2(low = "#4798ac", mid = "white", high = "#b5333b", midpoint = 0) +
+  theme_bw() +
+  theme(text = element_text(size = 12), legend.title = element_blank(),
+        plot.margin = ggplot2::margin(30,30,30,30),
+        panel.grid = element_blank(), legend.background = element_blank(),
+        axis.title.x = element_text(vjust = -4),
+        axis.title.y = element_text(vjust = 4))
